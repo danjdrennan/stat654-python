44a28df0fcaaa9911b03022ffa1d60e691ec9478
Daniel J. Drennan <danjdrennan@stat.tamu.edu>
2022-02-16 23:32:40 -0600

    formats code

--- src/plotting.py
+++ src/plotting.py
@@ -3,7 +3,16 @@ from typing import Union
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(x: np.ndarray, y: np.ndarray, figsize:tuple=(16,9), labels:dict=None, fname:str=None, *args, **kwargs)->None:
+
+def plot(
+    x: np.ndarray,
+    y: np.ndarray,
+    figsize: tuple = (16, 9),
+    labels: dict = None,
+    fname: str = None,
+    *args,
+    **kwargs
+) -> None:
     """
     Helper function for plotting data using plt.plot function.
     The plotter standardizes grids and figure sizes, and reduces overhead.
@@ -27,7 +36,7 @@ def plot(x: np.ndarray, y: np.ndarray, figsize:tuple=(16,9), labels:dict=None, f
         Note: Any format other than
         {
             'title': 'Some time',
-            'x_label': 'Some x label (with units), 
+            'x_label': 'Some x label (with units),
             'y_label': 'Some y label (with units)'
         }
         will not be used. Any one of these (or all of them) can be optionally skipped,
@@ -37,28 +46,27 @@ def plot(x: np.ndarray, y: np.ndarray, figsize:tuple=(16,9), labels:dict=None, f
         A filename to save the plot. The filename should include an extension.
     """
     plt.figure(figsize=figsize)
-    
+
     figwidth = figsize[0]
 
     # If labels are present, use them
     # Assume labels are correctly passed as a dict with expected keys
     if labels:
         try:
-            plt.title(labels['title'], fontsize=1.5*figwidth)
+            plt.title(labels["title"], fontsize=1.5 * figwidth)
         except Exception:
             pass
         try:
-            plt.xlabel(labels['x_label'], fontsize=1.25*figwidth)
+            plt.xlabel(labels["x_label"], fontsize=1.25 * figwidth)
         except Exception:
             pass
         try:
-            plt.ylabel(labels['y_label'], fontsize=1.25*figwidth)
+            plt.ylabel(labels["y_label"], fontsize=1.25 * figwidth)
         except Exception:
             pass
-    
+
     plt.plot(x, y, *args, **kwargs)
     plt.grid(alpha=0.2)
-    
+
     if fname:
-        plt.savefig(fname, format=fname.split('.')[-1], dpi=300)
-    
+        plt.savefig(fname, format=fname.split(".")[-1], dpi=300)
