197fb2cc2bb4ba55d1db32e001f6268f1f20f326
Daniel J. Drennan <danjdrennan@stat.tamu.edu>
2022-04-22 01:07:16 -0500

    Time series visualizations using base graphics library

---
+++ scripts/sample-visualizations.R
@@ -0,0 +1,32 @@
+N <- 100000
+# Going to construct two face time series
+x <- numeric(N)
+y <- numeric(N)
+
+x[1] <- rnorm(1, 0, 5)
+y[1] <- rnorm(1, 0, 5)
+for(t in 2:N){
+    x[t] <- 0.5 * x[t - 1] + rnorm(1, 0, 0.25)
+    y[t] <- 0.6 * y[t - 1] + rnorm(1, 0, 1.2)
+}
+
+# Plot traceplots
+par(mfrow = c(2, 1))
+plot(x, type = 'l')
+plot(y, type = 'l')
+
+# Plot ACF plots
+par(mfrow = c(1, 2))
+acf(x)
+acf(y)
+
+# Plot histograms
+# Thin chains appropriately based on the ACF plot
+par(mfrow = c(1, 2))
+hist(x[10 * 1:(N/10)], bins = 30, main = "x")
+hist(y[10 * 1:(N/10)], bins = 30, main = "y")
+
+# Or plot densities instead
+par(mfrow = c(1, 2))
+plot(density(x[10 * 1:(N/10)]), main = "x")
+plot(density(y[10 * 1:(N/10)]), main = "y")
