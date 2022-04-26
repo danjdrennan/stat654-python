442da175bcf8751a520eae227094b7c2693dcc09
Daniel J. Drennan <danjdrennan@stat.tamu.edu>
2022-04-25 23:43:43 -0500

    changes how subsequence is computed after thinning

--- scripts/sample-visualizations.R
+++ scripts/sample-visualizations.R
@@ -22,11 +22,22 @@ acf(y)
 
 # Plot histograms
 # Thin chains appropriately based on the ACF plot
+# The ACF plot shows autocorrelation to about seven samples,
+# so taking any subset of the chain which skips at least seven
+# samples will suffice to make independent samples.
 par(mfrow = c(1, 2))
 hist(x[10 * 1:(N/10)], bins = 30, main = "x")
 hist(y[10 * 1:(N/10)], bins = 30, main = "y")
 
+# Generate a subsequence for thinning the chain
+subseq <- seq(from=100, to = N, 10)
+subseq
+
 # Or plot densities instead
 par(mfrow = c(1, 2))
-plot(density(x[10 * 1:(N/10)]), main = "x")
-plot(density(y[10 * 1:(N/10)]), main = "y")
+plot(density(x[subseq]), main = "x")
+plot(density(y[subseq]), main = "y")
+
+# Compute the mean of the posterior samples (after thinning and burning in)
+mean(x[subseq])
+mean(y[subseq])
